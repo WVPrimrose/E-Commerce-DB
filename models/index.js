a5b73ab1a5e3816a07ff3714a373fb46369279c3
Katie Buchholz <kzbindy@hotmail.com>
2024-07-01 23:24:42 -0400

    minor fixes to the repo; don't remember what, but I'll keep working on it

--- models/index.js
+++ models/index.js
@@ -5,13 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category)
 // Categories have many Products
-
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag)
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongToMany(Product)
 module.exports = {
   Product,
   Category,
